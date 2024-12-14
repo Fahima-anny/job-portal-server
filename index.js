3140afcfe2dfc9851c66ad6df05244589060bfd7
Fahima-anny <fahima.info03@gmail.com>
2024-12-15 03:45:18 +0600

    crud

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb') ;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb') ;
 const cors = require('cors');
+require('dotenv').config() ;
 const app = express() ;
 const port = process.env.PORT || 3000 ;
 
 app.use(cors()) ;
 app.use(express.json()) ;
 
-// DB_USER = job_portal_admin
-// DB_PASS = 1vLb1mXyYbjxIKaM
 
-
-const uri = `mongodb+srv://<db_username>:<db_password>@cluster0.ohdc4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ohdc4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -29,9 +27,38 @@ async function run() {
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    const jobsCollection = client.db("jobPortal").collection("jobs")
+    const jobsApplicationCollection = client.db("jobPortal").collection("jobApplications")
+
+    // JOBS related api 
+    app.get("/jobs" , async (req, res) => {
+        const result = await jobsCollection.find().toArray() ;
+        res.send(result) ;
+    })
+
+    app.get("/jobs/:id", async(req, res) => {
+        const id = req.params.id ;
+        const query ={_id: new ObjectId(id)} ;
+        const result = await jobsCollection.findOne(query) ;
+        res.send(result) ; 
+    })
+
+
+app.post("/job-application" , async (req, res) => {
+    const application = req.body ;
+    const result = await jobsApplicationCollection.insertOne(application) ;
+    res.send(result) ;
+})
+
+
+
+
+
+
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
